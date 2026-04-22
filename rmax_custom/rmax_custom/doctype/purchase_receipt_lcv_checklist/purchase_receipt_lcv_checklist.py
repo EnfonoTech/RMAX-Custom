9b5d4e8aaba4a8ca9c02c5c0413ef1a3ad0fb97d
sayanthns <sayanth2k16@gmail.com>
2026-04-22 21:55:18 +0300

    fix(lcv): add controllers for child doctypes so migrate loads them

---
+++ rmax_custom/rmax_custom/doctype/purchase_receipt_lcv_checklist/purchase_receipt_lcv_checklist.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Rmax Custom and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class PurchaseReceiptLCVChecklist(Document):
+	pass
